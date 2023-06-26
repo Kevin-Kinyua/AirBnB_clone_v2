0c9c39f707ce09da858521c96951015234c02bc6
Kevin Wangome Kinyua <w.kevinkinyua@gmail.com>
2023-06-26 22:30:59 +0300

    3. MySQL setup development
    Write a script that prepares a MySQL server for the project:
    
    A database hbnb_dev_db
    A new user hbnb_dev (in localhost)
    The password of hbnb_dev should be set to hbnb_dev_pwd
    hbnb_dev should have all privileges on the database hbnb_dev_db (and only this database)
    hbnb_dev should have SELECT privilege on the database performance_schema (and only this database)
    If the database hbnb_dev_db or the user hbnb_dev already exists, your script should not fail

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,15 +1,12 @@
-<<<<<<< HEAD
--- Prepares a MySQL Test server for the project.
+-- this script prepares a MySQL server for the project
+-- create project testing database with the name : hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- creating new user named : hbnb_test with all privileges on the db hbnb_test_db
+-- with the password : hbnb_test_pwd if it dosen't exist
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
-=======
--- Creates a database called hbnb_test_db in the current MySQL server
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
--- creates the MySQL server user hbnb_test
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
--- Grants Permissions for user hbnb_test
-GRANT ALL ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
->>>>>>> 1c319d1a1f71365f47e891208cdbf1201a869328
+-- granting the SELECT privilege for the user hbnb_test on the db performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
+-- granting all privileges to the new user on hbnb_test_db
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
